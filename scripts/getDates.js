53f22a46fd029259c2557b83f76090492e2d122d
amandamigliorini <amanda.migliorini@hotmail.com>
2024-01-18 20:45:32 -0300

    Home Page Dark Mode and Hamburguer Button finished

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,11 @@
 // declaring variables to contain the last modified date and the current year for the footer
 let oLastModif = new Date(document.lastModified);
+//declaring navigation variables
+const hamburguerElement = document.querySelector("#menu");
+const navElements = document.querySelector(".navigation");
+//declaring dark mode variables
+const darkButton = document.getElementById('darkBtn');
+const main = document.querySelector('main')
 
 
 const options = {		
@@ -18,3 +24,14 @@ const lastModifiedElement = document.getElementById("lastModified");
 lastModifiedElement.textContent = oLastModif.toLocaleDateString("en-US", options);
 const yearElement = document.getElementById("year");
 year.textContent = currentYear;
+
+//adding class for open and close hamburguer menu
+hamburguerElement.addEventListener('click', () => {
+    navElements.classList.toggle('open');
+    hamburguerElement.classList.toggle('open');
+})
+
+//code for dark mode click
+darkButton.addEventListener('click',() =>{
+    main.classList.toggle('dark');
+})
