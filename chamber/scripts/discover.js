82c9fec095ce3b21c9bf2af07f63cfa65f1d9048
amandamigliorini <amanda.migliorini@hotmail.com>
2024-02-20 14:38:15 -0300

    few changes on discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -64,7 +64,7 @@ async function apiFetch(){
 function displayResults(data){
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
+    let desc = ` ${data.weather[0].description}`;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", data.weather[0].main );
     captionDesc.textContent = `${desc}`;
